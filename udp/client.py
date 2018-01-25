6459c4a6828f96d56c8a6e047ad4bae02bc8a7ce
an9wer <an9wer@gmail.com>
2018-01-25 21:51:13 +0800

    Add delay server/client

--- udp/client.py
+++ udp/client.py
@@ -1,6 +1,9 @@
 import socket
 
 
+MAX_BYTES = 65535
+
+
 def client():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     send_text = "hello world"
@@ -9,10 +12,48 @@ def client():
     print("the OS assigned me the address {}".format(sock.getsockname()))
 
     # block until receiving some data
-    recv_data, peer_address = sock.recvfrom(100)
+    recv_data, peer_address = sock.recvfrom(MAX_BYTES)
     recv_text = str(recv_data, "utf-8")
     print(recv_text, peer_address)
 
 
+def delay_client():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # why use connection here?
+    # ------------------------------------------------------------------------
+    # firstly:
+    # the connect() call lets the operating system know ahead of time the
+    # remote address to which you want to send packets so that you can simply
+    # supply data to the send() call and not have to repeat the server address
+    # again.
+    #
+    # secondly:
+    # once you have run connect(), the operating system will discard any
+    # incoming packets to your port whose return address does not match the
+    # address to which you have connected.
+    sock.connect(("127.0.0.1", 8080))
+    print("Client socket name is {}".format(sock.getsockname()))
+
+    delay = 0.1
+    send_text = "hello world"
+    send_data = bytes(send_text, "utf-8")
+    while True:
+        sock.send(send_data)
+        print("Waiting up to {} seconds for a reply".format(delay))
+        sock.settimeout(delay)
+        try:
+            recv_data = sock.recv(MAX_BYTES)
+        except socket.timeout:
+            delay *= 2
+            if delay > 2.0:
+                raise RuntimeError("I think the server is down")
+        else:
+            break
+
+    recv_text = str(send_data)
+    print("The server says {!r}".format(recv_text))
+
+
 if __name__ == "__main__":
-    client()
+    #client()
+    delay_client()
--- udp/server.py
+++ udp/server.py
@@ -1,5 +1,8 @@
 import socket
+import random
 
+
+MAX_BYTES = 65535
 
 def server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,7 +11,7 @@ def server():
 
     while True:
         # block until receiving some data
-        recv_data, peer_address = sock.recvfrom(2)
+        recv_data, peer_address = sock.recvfrom(MAX_BYTES)
         recv_text = str(recv_data, "utf-8")
         print("The client at {} says {!r}".format(peer_address, recv_text))
 
@@ -16,6 +19,22 @@ def server():
         send_data = bytes(send_text, "utf-8")
         sock.sendto(send_data, peer_address)
 
+def delay_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("127.0.0.1", 8080))
+    print("Listening at", sock.getsockname())
+    while True:
+        recv_data, peer_address = sock.recvfrom(MAX_BYTES)
+        if random.random() < 0.5:
+            print("Pretending to drop packet from {}".format(peer_address))
+            continue
+        recv_text = str(recv_data, "utf-8")
+        print("The client at {} says {!r}".format(peer_address, recv_text))
+        send_text = "Your data was {} bytes long".format(len(recv_data))
+        send_data = bytes(send_text, "utf-8")
+        sock.sendto(send_data, peer_address)
+
 
 if __name__ == "__main__":
-    server()
+    #server()
+    delay_server()
